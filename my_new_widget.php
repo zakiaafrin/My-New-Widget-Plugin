5d49cdf4bbf1cdb5b7809778c82ba0a1c833bfde
Zakia Afrin Jeme <zjeme@techlaunch.io>
2019-02-05 21:17:59 -0500

    Updated

--- my_new_widget.php
+++ my_new_widget.php
@@ -14,10 +14,7 @@ add_action('widgets_init', function(){
  });
 
 class My_New_Widget extends WP_Widget {
-  /**
-  * To create the example widget all four methods will be 
-  * nested inside this single instance of the WP_Widget class.
-  **/
+// class constructor
   public function __construct() {
     $widget_options = array( 
       'classname' => 'my_new_widget',
@@ -26,26 +23,35 @@ class My_New_Widget extends WP_Widget {
     parent::__construct( 'my_new_widget', 'My New Widget', $widget_options );
   }
 
+// output the widget content on the front end
 public function widget( $args, $instance ) {
     $title = apply_filters( 'widget_title', $instance[ 'title' ] );
     $blog_title = get_bloginfo( 'name' );
     $tagline = get_bloginfo( 'description' );
     echo $args['before_widget'] . $args['before_title'] . $title . $args['after_title']; ?>
-  
-    <p><strong>Site Name:</strong> <?php echo $blog_title ?></p>
-    <p><strong>Tagline:</strong> <?php echo $tagline ?></p>
-  
-    <?php echo $args['after_widget'];
-  }
 
+  <p><strong>Site Name:</strong>
+  <?php echo $blog_title ?>
+  </p>
+  <p><strong>Tagline:</strong>
+  <?php echo $tagline ?>
+  </p>
+
+  <?php echo $args['after_widget'];
+  }
+  
+// output the option form in admin Widges screen
   public function form( $instance ) {
     $title = ! empty( $instance['title'] ) ? $instance['title'] : ''; ?>
-    <p>
-      <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
-      <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" value="<?php echo esc_attr( $title ); ?>" />
-    </p><?php 
+  <p>
+  <label for="<?php echo $this->get_field_id( 'title' ); ?>">Title:</label>
+  <input type="text" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>"
+    value="<?php echo esc_attr( $title ); ?>" />
+  </p>
+  <?php 
   }
 
+   // save options
   public function update( $new_instance, $old_instance ) {
     $instance = $old_instance;
     $instance[ 'title' ] = strip_tags( $new_instance[ 'title' ] );
